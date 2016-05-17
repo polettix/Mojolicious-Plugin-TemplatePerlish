ecb369213c1b91def9e9bd2d85eb5ab36b3ba787
Flavio Poletti <flavio@polettix.it>
2016-05-17 08:58:36 +0200

    advanced, most stuff in

--- cpanfile
+++ cpanfile
@@ -1,1 +1,6 @@
-# Put your requirements here...
+requires 'Template::Perlish', '1.52';
+requires 'Mojolicious',       '6.0';
+
+on develop => sub {
+   requires 'Path::Tiny', '0.084';
+};
---
+++ support/podversion.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Template::Perlish ();
+use Path::Tiny;
+use 5.010;
+
+my ($distro, $version) = @ARGV;
+my $podfile =
+  path($distro)->child(qw< lib Mojolicious Plugin TemplatePerlish.pod >);
+my $readme = path($distro)->child('README');
+
+my $tp = Template::Perlish->new(
+   start     => '{{[',
+   stop      => ']}}',
+   variables => {
+      distro  => $distro,
+      version => $version,
+   },
+);
+
+for my $file ($podfile, $readme) {
+   my $rendered = $tp->process($file->slurp_raw());
+   $file->spew_raw($rendered);
+}
